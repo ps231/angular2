d66dd2db434ac36a716c2ffe20ffd2e18b4e32d2
Prasad Shindikar <prasad.shindikar@cdk.com>
2016-11-22 15:52:04 -0800

    simple basic routing example

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailsComponent } from './hero-details.component';
@@ -8,10 +9,20 @@ import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, 
+  				  FormsModule,
+  				  RouterModule.forRoot([
+  				  {
+    				path: 'heroes',
+    				component: HeroesComponent
+  				  }
+				  ])
+				],
   declarations: [ AppComponent, HeroDetailsComponent, HeroesComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService ]
 })
+
+
 
 export class AppModule { }
