aaf03b4f77bda8b262930449ed8f05c870921bfb
Prasad Shindikar <prasad.shindikar@cdk.com>
2016-11-22 15:11:13 -0800

    using a promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -77,7 +77,7 @@ export class AppComponent implements OnInit{
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
